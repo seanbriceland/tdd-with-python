dacf588b1f950da9fb760054dd0557ecf1ca6ef1
Sean Briceland <spb1014@gmail.com>
2014-07-09 17:08:27 -0400

    updated the functional tests for different lists for different users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,22 +2,20 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(1)
-    
 
     def tearDown(self):
         self.browser.quit()
-
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # go to homepage
@@ -37,9 +35,12 @@ class NewVisitorTest(LiveServerTestCase):
         
         # verify user can enter a to-do item
         inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
 
-        # verify page is updated and lists to-do item
+        # when ENTER is pressed, user is taken to a new URL,
+        # and now the page lists the item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        user_list_url = self.browser.current_url
+        self.assertRegex(user_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # verify there is an input again to add an item
@@ -52,11 +53,35 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # User wonders if the site will remember the to-do list
-        # they take note of the URL 
-        self.fail('Finish the test!')
+        # Now a new user comes to the site
 
-        # visit the url and check to-do list is still there
-        
+        ## We use a new browser session to make sure no information
+        ## of user 1 is coming though from cookies, etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # User 2 visits the home page. There is no sign of User 1s list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # User 2 starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # User 2 gets his own unique URL
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/lists/.+')
+        self.assertNotEqual(user2_list_url, user_list_url)
+
+        # Once User 2 gets to their own list page
+        # there is no Items of User 1's list in their list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy Milk', page_text)
+
+        self.fail('Finish the test!')
         # All tests run
         browser.quit()
