994f3209ff2439e57455461afb40f7eb1cb528b9
Sean Briceland <spb1014@gmail.com>
2014-07-09 13:58:23 -0400

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,22 +11,36 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
-    def test_can_start_a_list_and_retreive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # go to homepage
         self.browser.get('http://localhost:8000')
         
         # verify title of homepage
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
-        
-        # verify user can enter a to-do item
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
         
         # verify user input is available for new to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         
+        # verify user can enter a to-do item
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
         # verify page is updated and lists to-do item
+        table = self.browser.find_element_by_id()
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         
         # verify there is an input again to add an item
         # user enters another to-do item
+        self.fail('Finish the test!')
         
         # page updates again, showing both items in list
         
@@ -39,4 +54,3 @@ class NewVisitorTest(unittest.TestCase):
     
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-        
