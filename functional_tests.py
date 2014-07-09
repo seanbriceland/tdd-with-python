92231cb61efd01d36bd92a648e5889226244c492
Sean Briceland <spb1014@gmail.com>
2014-07-09 14:23:19 -0400

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -32,10 +32,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # verify page is updated and lists to-do item
-        table = self.browser.find_element_by_id()
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not not appear in table"
         )
         
         # verify there is an input again to add an item
